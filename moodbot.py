8d44986f8ccc7d0021ddf81ae9064df07f3f133a
Aviad Nissel <aviadnissel@gmail.com>
2019-03-21 21:00:50 +0200

    broken irc

--- moodbot.py
+++ moodbot.py
@@ -1,27 +1,23 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-from twitchio.ext import commands
+import json
+import asyncio
+import pydle
 
+# Simple echo bot.
+class MyOwnBot(pydle.Client):
+    async def on_connect(self):
+        print("Connected!")
+        await super().on_connect()
+        await self.join("#danielfenner")
 
-class Bot(commands.Bot):
-
-    def __init__(self):
-        super().__init__(irc_token='...', client_id='...', nick='...', prefix='!',
-                         initial_channels=['...'])
-
-    # Events don't need decorators when subclassed
-    async def event_ready(self):
-        print(f'Ready | {self.nick}')
-
-    async def event_message(self, message):
-        print(message.content)
-        await self.handle_commands(message)
-
-    # Commands use a different decorator
-    @commands.command(name='test')
-    async def my_command(self, ctx):
-        await ctx.send(f'Hello {ctx.author.name}!')
+    # Override from base class
+    async def on_message(self, timestamp, tags, channel, user, message):
+        print(message)
 
 
-bot = Bot()
-bot.run()
+client = MyOwnBot('moodbottv')
+client.run('irc.chat.twitch.tv')
+
+cfg = json.loads(open("bot.cfg", "rb").read())
+oauth = cfg["OAuth"]
